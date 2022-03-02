00b66e8cf35b7bb7d62cd20314e6ab5b773b06db
ozymandiaslone <saltsperkins@gmail.com>
2022-03-01 23:20:32 -0600

    filename changed

--- Sara V.0/main.py
+++ Sara V.0/main.py
@@ -5,7 +5,7 @@ from email.message import EmailMessage
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
-from clientClass import *
+from ClientClass import *
 import pickle
 import random
 import os
